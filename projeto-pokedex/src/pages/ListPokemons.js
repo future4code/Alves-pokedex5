d994d23ef950f2167ac0f99d3aa8a6ef6ee04443
Klebson Bicalho de Sousa <klebson.sis@gmail.com>
2022-07-19 18:54:58 -0300

    Lista de pokemons renderizando

--- projeto-pokedex/src/pages/ListPokemons.js
+++ projeto-pokedex/src/pages/ListPokemons.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { BASE_URL } from "../constants/BASE_URL.JS";
+import { BASE_URL } from "../constants/BASE_URL.js";
 import { goToDetailPokemon } from "../routes/coordinator";
 
 function ListPokemons() {
   const navigate = useNavigate();
   const [listPokemons, setListPokemons] = useState([]);
+  const [novaListaPokemon, setNovaListaPokemon] = useState([]);
 
   useEffect(() => {
     axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
+      .get(`${BASE_URL}pokemon?limit=20&offset=0`)
       .then((response) => {
         // console.log(response.data.results)
         setListPokemons(response.data.results);
@@ -19,42 +20,41 @@ function ListPokemons() {
         // console.log(error.response)
       });
   }, []);
-
-  const listPokemon =
-    listPokemons &&
-    listPokemons.map((pokemon) => {
-      // return <div key={pokemon.name}>
-      //   <p>{pokemon.name}</p>
-      //   <img src={pokemon.url} alt="Imagem do pokemon"/>
-      // </div>
-
+  useEffect(() => {
+    const listaPokemon = [];
+    listPokemons.forEach((pokemon) => {
       axios
-        .get(pokemon.url)
+        .get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
         .then((response) => {
-          console.log(response.data);
+          listaPokemon.push(response.data);
+          if (listaPokemon.length === 20) {
+            setNovaListaPokemon(listaPokemon);
+          }
         })
-        .catch((error) => {
-          console.log(error.response);
-        });
+        .catch((error) => {});
+    });
+  }, [listPokemons]);
+  console.log(novaListaPokemon);
+
+  
+  const infoPokemon =
+    novaListaPokemon &&
+    novaListaPokemon.map((pokemon) => {
+      console.log(pokemon.types)
       return (
-        <div key={pokemon.name}>
+        <div key={pokemon.id}>
           <p>{pokemon.name}</p>
-          {/* <img src={} alt="Imagem do pokemon" /> */}
+          <p>{pokemon.types["name"]}</p>
+          <img src={pokemon.sprites.front_default} alt="Imagem do pokemon" />
         </div>
       );
     });
-  // axios.get(`https://pokeapi.co/api/v2/${listPokemon}`)
-  // .then((response) => {
-  //   console.log(response.data)
-  // }).catch((error) => {
-  //   console.log(error.response)
-  // })
-
-  console.log(listPokemon);
+    
   return (
     <div>
       <h1>Lista de Pokemons</h1>
       <button onClick={() => goToDetailPokemon(navigate)}>Detalhes</button>
+      {infoPokemon}
     </div>
   );
 }
