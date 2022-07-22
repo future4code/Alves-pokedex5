6669b0fa5668abf565669302f49a32670e056cc8
Gilead Raab <gileadraab@gmail.com>
2022-07-22 16:56:31 -0300

    algumas atualizações na renderização da pokedex, adicionadas algumas logicas sobre captura/remover pokemons

--- projeto-pokedex/src/pages/Pokedex.js
+++ projeto-pokedex/src/pages/Pokedex.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { GlobalContext } from "../global/GlobalContext";
 import {goToDetailPokemon, goToBack} from "../routes/coordinator"
-import { CardPokemon, ContainerPokemon } from "../components/StyledListPokemons";
+import { CardPokemon, ContainerPokemon, CardButton, CardIdName, Type } from "../components/StyledListPokemons";
 
 function Poquedex() {
 
@@ -13,20 +13,31 @@ function Poquedex() {
 
   const myPokemons = pokedex.map((pokemon) => {
     return (
-    <CardPokemon key={pokemon.id}>          
-    <p>{pokemon.name}</p>
-  
-    <img src={pokemon.sprites.front_default} alt="Imagem do pokemon" />
-    <div>
-      {pokemon.types.map((type, index) => {
-        return <div key={index}>{type.type.name}</div>
-      })}
-    </div>
-    <button onClick={() => goToDetailPokemon(navigate)}>Detalhes</button>          
-    <button onClick >Remover</button>
-  </CardPokemon>
-    )
-  })
+      <CardPokemon key={pokemon.id} typePokemon={pokemon.types[0].type.name}>
+        <img
+          src={pokemon.sprites.other.dream_world.front_default}
+          alt="Imagem do pokemon"
+        />
+        <CardIdName>
+          #{pokemon.id}
+          <h3>{pokemon.name}</h3>
+        </CardIdName>
+        <Type typePokemon={pokemon.types[0].type.name}>                      
+          {pokemon.types.map((type, index) => {      
+            return <div key={index}>{type.type.name}</div>;
+          })}
+        </Type>
+        <CardButton>
+          <button onClick={() => goToDetailPokemon(navigate)}>
+            Detalhes
+          </button>
+          <button>
+            Remover
+          </button>
+        </CardButton>
+      </CardPokemon>
+    );
+  });
 
 
 
@@ -34,7 +45,6 @@ function Poquedex() {
     <div>
       <h1>Meus Pokémons</h1>
       <ContainerPokemon>{myPokemons}</ContainerPokemon>
-      <button onClick={() => goToDetailPokemon(navigate)}>Detalhes</button>
     </div>
   );
 }
